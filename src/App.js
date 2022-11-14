35f7cd120055e56eafbcb010c1b67ad781dc41a4
Jahirul Islam <jahirul@elyranur.com>
2022-11-15 00:36:02 +0600

    some practice

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button, Spinner } from 'react-bootstrap';
+import GridCard from './components/GirdCard/GridCard';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button className='btn btn-warning'>Submit</Button>
+      <Button variant='danger' className='mx-4 my-5'>My button</Button>
+      <Spinner animation="grow" variant="warning" />
+      <GridCard></GridCard>
     </div>
   );
 }
